ebf2187811ab3dfce71eaf35e6124c75fd80a12e
cbesangeeth <cbe.sangeeth@gmail.com>
2021-04-11 20:20:05 +0530

    update config

--- config/dev.js
+++ config/dev.js
@@ -1,17 +1,18 @@
 module.exports = {
-    db: {
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
-      schema: "public",
-      host: process.env.HOST,
-      dialect: "postgres",
-      port: process.env.PORT,
-      driver: "pg",
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
+  db: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    schema: "public",
+    host: process.env.HOST,
+    dialect: "postgres",
+    port: process.env.PORT,
+    driver: "pg",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
       }
-    },
+    }
+  },
 }
