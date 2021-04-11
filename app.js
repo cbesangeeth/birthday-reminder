caf6f74475cd4ef7d5a65c423c3749769a48ac4a
cbesangeeth <cbe.sangeeth@gmail.com>
2021-04-11 20:04:27 +0530

    fix imports

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./handlers/v1/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
