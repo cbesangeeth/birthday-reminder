daaa9aa80dfbc7ecd1d0e58534bcdc7c1a617d2f
cbesangeeth <cbe.sangeeth@gmail.com>
2021-04-12 21:19:13 +0530

    fix typo

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
 
   res.set({
-    "Access-Control-Allow-Origin", "*",
+    "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
     "Access-Control-Allow-Methods": "GET,POST,PATCH,DELETE",
     "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
